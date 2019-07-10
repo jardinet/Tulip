6d816a1ae183a05af5e2ea8ef3a4c5f46b6c4f02
Marie WRAGG <mwragg@student.42.fr>
2019-07-10 02:54:29 +0200

    I CAN ESCAPE NOOOOW

--- affichage.c
+++ affichage.c
@@ -6,13 +6,23 @@
 /*   By: mwragg <mwragg@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/10 01:51:49 by mwragg            #+#    #+#             */
-/*   Updated: 2019/07/10 02:17:35 by mwragg           ###   ########.fr       */
+/*   Updated: 2019/07/10 02:51:53 by mwragg           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 #include <stdio.h> // TO REMOVE
-void affichage() 
+
+int		key_events(int key, void *ptr)
+{
+(void)ptr;
+if (key == 53)
+	exit(0);
+printf("Value of key %d\n", key);	
+return (0);
+}
+
+void	affichage() 
 {
 	void *mlx_id;    // identitifiant de la connexion au serveur graphique
 	void *win_ptr;
@@ -27,6 +37,6 @@ void affichage()
 	win_ptr = mlx_new_window(mlx_id, WINX, WINY, "Window title: The Winx adventures");
 	mlx_img = mlx_new_image(mlx_id, WINX, WINY);
 	buff = mlx_get_data_addr(mlx_img, &bpp, &sizeline, &endian);
-	printf("bpp : \t%d\nsizeline\t%d\nendian\t%d\n", bpp, sizeline, endian);
-	printf("Cause Insisted: %p\n", win_ptr); 
+	mlx_key_hook(win_ptr, key_events, (void*) 0);
+	mlx_loop(mlx_id);
 }
